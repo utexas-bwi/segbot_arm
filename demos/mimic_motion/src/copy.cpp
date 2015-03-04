7f50300ee953e640b3fc8daea56a5a4c4f39cae8
maxsvetlik <maxsvetlik@gmail.com>
2015-03-04 17:14:30 -0600

    finished fingers

--- demos/mimic_motion/src/copy.cpp
+++ demos/mimic_motion/src/copy.cpp
@@ -1,8 +1,8 @@
 #include <ros/ros.h>
-#include <signal.h> 
+#include <signal.h>
 #include <iostream>
 #include <vector>
-#include <math.h> 
+#include <math.h>
 #include <cstdlib>
 #include <std_msgs/String.h>
 
@@ -17,6 +17,10 @@
 
 
 using namespace std;
+
+#define FINGER_FULLY_OPENED 6
+#define FINGER_FULLY_CLOSED 7300
+
 
 sensor_msgs::JointState current_state;
 sensor_msgs::JointState current_effort;
@@ -49,15 +53,15 @@ void fingers_cb (const jaco_msgs::FingerPosition msg) {
 }
 
 
-void move() {
+void movePose() {
   actionlib::SimpleActionClient<jaco_msgs::ArmPoseAction> ac("/mico_arm_driver/arm_pose/arm_pose", true);
-  
+
   jaco_msgs::ArmPoseGoal goalPose;
-  
+
   // Set goal pose coordinates
-  
+
   goalPose.pose.header.frame_id = "mico_api_origin";
-  
+
   goalPose.pose.pose.position.x = current_pose.pose.position.x;
   goalPose.pose.pose.position.y = current_pose.pose.position.y;
   goalPose.pose.pose.position.z = current_pose.pose.position.z - 0.1;
@@ -65,7 +69,7 @@ void move() {
   goalPose.pose.pose.orientation.y = current_pose.pose.orientation.y;
   goalPose.pose.pose.orientation.z = current_pose.pose.orientation.z;
   goalPose.pose.pose.orientation.w = current_pose.pose.orientation.w;
-  
+
   ac.waitForServer();
   ROS_DEBUG("Waiting for server.");
   //finally, send goal and wait
@@ -75,34 +79,58 @@ void move() {
 
 }
 
+// Range = [6, 7300] ([open, close])
+void moveFinger(int finger_value) {
+    actionlib::SimpleActionClient<jaco_msgs::SetFingersPositionAction> ac("/mico_arm_driver/fingers/finger_positions", true);
+
+    jaco_msgs::SetFingersPositionGoal goalFinger;
+
+    goalFinger.fingers.finger1 = finger_value;
+    goalFinger.fingers.finger2 = finger_value;
+    // Not used for our arm
+    goalFinger.fingers.finger3 = 0;
+    ROS_INFO(".");
+    ac.waitForServer();
+    ROS_INFO("..");
+    ac.sendGoal(goalFinger);
+    ROS_INFO("...");
+    ac.waitForResult();
+}
+
 int main(int argc, char **argv) {
 
   // Intialize ROS with this node name
   ros::init(argc, argv, "mimic_motion");
 
   ros::NodeHandle n;
-  
+
   //create subscriber to joint angles
   ros::Subscriber sub_angles = n.subscribe ("/mico_arm_driver/out/joint_state", 1, joint_state_cb);
-  
+
   //create subscriber to joint torques
   ros::Subscriber sub_torques = n.subscribe ("/mico_arm_driver/out/joint_efforts", 1, joint_effort_cb);
-  
+
   //create subscriber to tool position topic
   ros::Subscriber sub_tool = n.subscribe("/mico_arm_driver/out/tool_position", 1, toolpos_cb);
 
   //subscriber for fingers
   ros::Subscriber sub_finger = n.subscribe("/mico_arm_driver/out/finger_position", 1, fingers_cb);
 
-  //  while (ros::ok()) {
-  ros::Duration(5).sleep();  
-  ros::spinOnce();
-    
-    move();
-    
-    //ROS_INFO_STREAM(current_pose.pose.position.z);
-    //  }
-
-    //ROS_INFO_STREAM("Error: robot not OK");
+  unsigned int finger_open_close_toggle = 0;
+  while (ros::ok()) {
+      ros::spinOnce();
+      ros::Duration(5).sleep();
+      ROS_INFO("Iteration = %d", finger_open_close_toggle);
+      if (finger_open_close_toggle % 2 == 0) {
+          moveFinger(FINGER_FULLY_OPENED);
+          ROS_INFO("Opening...");
+      } else {
+          moveFinger(FINGER_FULLY_CLOSED);
+          ROS_INFO("Closing...");
+      }
+      finger_open_close_toggle++;
+  }
+//  movePose();
+//  moveFinger(int finger_value);
   return 0;
 }
