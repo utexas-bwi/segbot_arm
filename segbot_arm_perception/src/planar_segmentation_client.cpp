89245d8be8b5b71ae21a5a526527777f7cf52c6f
Maxwell Svetlik <maxsvetlik@utexas.edu>
2015-03-19 16:39:03 -0500

    added service request numberOfPlanes testing functionality

--- segbot_arm_perception/src/planar_segmentation_client.cpp
+++ segbot_arm_perception/src/planar_segmentation_client.cpp
@@ -11,18 +11,20 @@ int main(int argc, char **argv)
   segbot_arm_perception::PlanarSegmentation srv_msg;
   ros::Publisher pub = n.advertise<sensor_msgs::PointCloud2> ("segmented_pc", 1);
 
+  //Test service requests
   srv_msg.request.excludePlane = false;
+  srv_msg.request.numberOfPlanes = 0;
   char in = ' ';
   while(in != 'q' && ros::ok()){
     std::cout << "Enter q to quit, any other character to call the service." << std::endl;
     std::cin >> in; 
     if (client.call(srv_msg))
     {
-      ROS_INFO("Got a response. Publishing to /segmented_pc");
       std::vector<sensor_msgs::PointCloud2> res = srv_msg.response.clouds;
+      ROS_INFO("Got %lu clouds. Publishing to /segmented_pc", res.size());
       for(int i = 0; i < res.size(); i++){
-        pub.publish(res.at(i));
         std::cin >> in;    //cycle through all the pointclouds. Useful for visualizing in rviz
+        pub.publish(res.at(i));
       }
     }
     else
