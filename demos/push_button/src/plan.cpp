1b71d428d6bec8f0fd1817a97a6ef7fbc6fd2a58
Jivko Sinapov <jsinapov@cs.utexas.edu>
2015-03-19 20:31:31 -0500

    added push_button package

---
+++ demos/push_button/src/plan.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+#include <ros/ros.h>
+#include <ros/package.h>
+
+int
+pressEntertoContinue()
+{
+    std::cout << "Press ENTER to continue..." << std::endl;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    return 0;
+}
+
+
+int
+zFilter (plc::PointCloud<pcl::PointXYZ> inputPointCloud)
+{
+    pcl::PassThrough<pcl::PointXYZ> pass;
+    pass.setInputCloiud(inputPointCloud);
+    pass.setFilterFieldName("z");
+    pass.setFilterLimit(0.0, );
+}
+
+
+int
+main (int argc, char** argv)
+{
+    pcl::PCLPointCloud2::Ptr cloud_blob (new pcl::PCLPointCloud2), cloud_filtered_blob (new pcl::PCLPointCloud2);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>), cloud_p (new pcl::PointCloud<pcl::PointXYZ>), cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
+
+    // Fill in the cloud data
+    pcl::PCDReader reader;
+//    std::string pathName = ros::package::getPath("push_button");
+    std::string pathName = "/nishome/bwi/catkin_ws/src/segbot_arm/demos/push_button/src/table_scene_lms400.pcd";
+    reader.read (pathName, *cloud_blob);
+
+    std::cerr << "PointCloud before filtering: " << cloud_blob->width * cloud_blob->height << " data points." << std::endl;
+
+    // Create the filtering object: downsample the dataset using a leaf size of 1cm
+    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+    sor.setInputCloud (cloud_blob);
+    sor.setLeafSize (0.01f, 0.01f, 0.01f);
+    sor.filter (*cloud_filtered_blob);
+
+    // Convert to the templated PointCloud
+    pcl::fromPCLPointCloud2 (*cloud_filtered_blob, *cloud_filtered);
+
+    std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height << " data points." << std::endl;
+
+    // Write the downsampled version to disk
+    pcl::PCDWriter writer;
+    std::string pathNameWrite = "/nishome/bwi/catkin_ws/src/segbot_arm/demos/push_button/src/table_scene_lms400_downsampled.pcd";
+    writer.write<pcl::PointXYZ> (pathNameWrite, *cloud_filtered, false);
+
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
+    // Create the segmentation object
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    // Optional
+    seg.setOptimizeCoefficients (true);
+    // Mandatory
+    seg.setModelType (pcl::SACMODEL_PLANE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setMaxIterations (1000);
+    seg.setDistanceThreshold (0.01);
+
+    std::cerr << "Start Cloud Viewer..." << std::endl;
+    std::cerr << "Original point cloud" << std::endl;
+
+    // pcl::visualization::CloudViewer viewer ("plane segmentation");
+    // viewer.showCloud (cloud_filtered);
+    // while (!viewer.wasStopped ());
+
+    // Create the filtering object
+    pcl::ExtractIndices<pcl::PointXYZ> extract;
+
+    int i = 0, nr_points = (int) cloud_filtered->points.size ();
+    // While 30% of the original cloud is still there
+    while (cloud_filtered->points.size () > 0.3 * nr_points)
+    {
+        // Segment the largest planar component from the remaining cloud
+        seg.setInputCloud (cloud_filtered);
+        seg.segment (*inliers, *coefficients);
+        if (inliers->indices.size () == 0)
+        {
+            std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
+            break;
+        }
+
+        // Extract the inliers
+        extract.setInputCloud (cloud_filtered);
+        extract.setIndices (inliers);
+        extract.setNegative (false);
+        extract.filter (*cloud_p);
+        std::cerr << "PointCloud representing the planar component: " << cloud_p->width * cloud_p->height << " data points." << std::endl;
+
+        std::cerr << "Start Cloud Viewer..." << std::endl;
+        pcl::visualization::CloudViewer viewer ("plane segmentation");
+        viewer.showCloud (cloud_p);
+        while (!viewer.wasStopped ());
+
+        std::stringstream ss;
+        ss << "table_scene_lms400_plane_" << i << ".pcd";
+        writer.write<pcl::PointXYZ> (ss.str (), *cloud_p, false);
+
+        // Create the filtering object
+        extract.setNegative (true);
+        extract.filter (*cloud_f);
+        cloud_filtered.swap (cloud_f);
+        i++;
+    }
+
+    return (0);
+}
