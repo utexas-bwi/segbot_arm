d7cefc7436a6d4f21e4397fa64c6986579c6ea0f
maxsvetlik <maxsvetlik@gmail.com>
2015-03-28 20:48:56 -0500

    added nearest neighbor search to find the closest point in the pc to the end effector

--- demos/draw_demo/src/approach_board.cpp
+++ demos/draw_demo/src/approach_board.cpp
@@ -17,19 +17,32 @@
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/conversions.h>
 #include <pcl_ros/transforms.h>
+#include <pcl/kdtree/kdtree_flann.h>
 
 
 /* Author: Maxwell Svetlik
- * Current state: finds centroid and transforms frame
- * Todo: find closest point to end effector, transform frame, approach
- * will require format of drawing demo code for drawing. 
- * /
+ * Current state: finds closest point to end effector through transforms and KdTree search
+ * Todo: compute the transform of the orientation of the plane, so the EF can approach the plane normal
+ * 
+ * Write something (will require format of drawing demo code for drawing)
+ */
 
+
+double cur_x, cur_y, cur_z;
 
 //true if Ctrl-C is pressed
 bool g_caught_sigint=false;
 
-/* what happens when ctr-c is pressed */
+
+
+void toolpos_cb(const geometry_msgs::PoseStamped &msg){
+	geometry_msgs::PoseStamped current = msg;
+	cur_x = current.pose.position.x;
+	cur_y = current.pose.position.y;
+	cur_z = current.pose.position.z;
+}
+
+
 void sig_handler(int sig)
 {
   g_caught_sigint = true;
@@ -47,7 +60,9 @@ int main (int argc, char** argv)
 	ros::ServiceClient ps_client = n.serviceClient<segbot_arm_perception::PlanarSegmentation>("PlanarSegmentation");
 	//pose pub
 	ros::Publisher pose_pub = n.advertise<geometry_msgs::PoseStamped>("approach_board/pose", 10);
-
+	//create subscriber to tool position topic
+	ros::Subscriber sub_tool = n.subscribe("/mico_arm_driver/out/tool_position", 1, toolpos_cb);
+	
 	tf::TransformListener listener;
 	signal(SIGINT, sig_handler);
 	
@@ -71,29 +86,59 @@ int main (int argc, char** argv)
 					pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane(new pcl::PointCloud<pcl::PointXYZ>);
 					pcl::fromPCLPointCloud2(temp,*cloud_plane);
 					
-					//cloud_plane = (res.at(0));
-					Eigen::Vector4f centroid;
-					pcl::compute3DCentroid(*cloud_plane, centroid);
+					//Transform EF XYZ to /camera/depth_registered
+					geometry_msgs::Pose ef_pose;
+					ef_pose.position.x = cur_x;
+					ef_pose.position.y = cur_y;
+					ef_pose.position.z = cur_z;
+					ef_pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0,0,-3.14/2);
+					geometry_msgs::PoseStamped ef_mico;
 
-					//transform pose to map
-					geometry_msgs::Pose target;
-					target.position.x=centroid(0);
-					target.position.y=centroid(1);
-					target.position.z=centroid(2);
-					target.orientation = tf::createQuaternionMsgFromRollPitchYaw(0,0,-3.14/2);
-					geometry_msgs::PoseStamped pose_in;
+					ef_mico.header.frame_id = "mico_api_origin";
+					ef_mico.header.stamp = ros::Time(0);
+					ef_mico.pose = ef_pose;
 
-					pose_in.header.frame_id = cloud_plane->header.frame_id;
-					pose_in.header.stamp = ros::Time(0);
-					pose_in.pose = target;
+					geometry_msgs::PoseStamped ef_camera;
+					listener.waitForTransform("mico_api_origin", cloud_plane->header.frame_id, ros::Time(0), ros::Duration(3.0));
+					listener.transformPose("mico_api_origin", ef_mico, ef_camera);
+					
+					
+					//Find closest point to EF
+					pcl::PointXYZ targetPoint;
+					int K = 5; //K nearest neighbors
+					std::vector<int> pointIdxNKNSearch(K);
+					std::vector<float> pointNKNSquaredDistance(K);
+					targetPoint.x = ef_camera.pose.position.x;
+					targetPoint.y = ef_camera.pose.position.y;
+					targetPoint.z = ef_camera.pose.position.z;
+					
+					pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
+					kdtree.setInputCloud(cloud_plane);
+					
+					if(kdtree.nearestKSearch(targetPoint, K, pointIdxNKNSearch, pointNKNSquaredDistance) > 0){
+						//transform pose to mico
+						geometry_msgs::Pose target;
+						target.position.x = cloud_plane->points[pointIdxNKNSearch[0]].x;
+						target.position.y = cloud_plane->points[pointIdxNKNSearch[0]].y;
+						target.position.z = cloud_plane->points[pointIdxNKNSearch[0]].z;
+						target.orientation = tf::createQuaternionMsgFromRollPitchYaw(0,0,-3.14/2);
+						geometry_msgs::PoseStamped pose_in;
 
-					geometry_msgs::PoseStamped pose_out;
-					listener.waitForTransform(cloud_plane->header.frame_id, "mico_api_origin", ros::Time(0), ros::Duration(3.0));
-					listener.transformPose("mico_api_origin", pose_in, pose_out);
+						pose_in.header.frame_id = cloud_plane->header.frame_id;
+						pose_in.header.stamp = ros::Time(0);
+						pose_in.pose = target;
 
-					pose_out.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0,-3.14/2,0);
-					std::cout << pose_out.pose.position.x << " " << pose_out.pose.position.y << " " << pose_out.pose.position.z << std::endl;
-					//pose_pub.publish(pose_out);
+						geometry_msgs::PoseStamped pose_out;
+						listener.waitForTransform(cloud_plane->header.frame_id, "mico_api_origin", ros::Time(0), ros::Duration(3.0));
+						listener.transformPose("mico_api_origin", pose_in, pose_out);
+
+						pose_out.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0,-3.14/2,0);
+						std::cout << "The target approach point in the arm frame: ";
+						std::cout << pose_out.pose.position.x << " " << pose_out.pose.position.y << " " << pose_out.pose.position.z << std::endl;
+						//pose_pub.publish(pose_out);
+					}
+					else
+						std::cout << "Unable to find closest point." << std::endl;
 				}
 				else
 					std::cout << "The service returned 0 point clouds." << std::endl;
