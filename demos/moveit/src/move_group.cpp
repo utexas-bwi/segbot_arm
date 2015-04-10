9d8a9a97ae1752e2bd9eb003a4fd90103618f620
maxsvetlik <maxsvetlik@gmail.com>
2015-04-10 18:16:14 -0500

    added object and object collision avoidance

--- demos/moveit/src/move_group.cpp
+++ demos/moveit/src/move_group.cpp
@@ -4,14 +4,17 @@
 #include <moveit_msgs/DisplayTrajectory.h>
 #include <moveit_msgs/AttachedCollisionObject.h>
 #include <moveit_msgs/CollisionObject.h>
+//used for assignment of vector
+#include <boost/assign/std/vector.hpp>
+using namespace boost::assign;
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "move_group_interface_tutorial");
 	ros::NodeHandle node_handle;
 	ros::AsyncSpinner spinner(1);
 	spinner.start();
-	/* This sleep is ONLY to allow Rviz to come up */
-	sleep(20.0);
+	
+	std::cout << "Please ensure that demo.launch has been run!" << std::endl;
 	// BEGIN_TUTORIAL
 	//
 	// Setup
@@ -25,7 +28,7 @@ int main(int argc, char **argv)
 	// class to deal directly with the world.
 	moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 	// Create a publisher for visualizing plans in Rviz.
-	ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/monitored_planning_scene", 1, true);
+	ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
 	moveit_msgs::DisplayTrajectory display_trajectory;
 	// Getting Basic Information
 	// ^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -35,6 +38,10 @@ int main(int argc, char **argv)
 	// We can also print the name of the end-effector link for this group.
 	ROS_INFO("Reference frame: %s", group.getEndEffectorLink().c_str());
 	//create a target goal pose
+	
+
+	//cartesianal point
+	/*
 	geometry_msgs::Pose target_pose1;
 	target_pose1.orientation.w = 1.0;
 	target_pose1.position.x = 0.28;
@@ -44,30 +51,93 @@ int main(int argc, char **argv)
 	//group.setPoseTarget(target_pose1.position.x, target_pose1.position.y, target_pose1.position.z, "mico_link_hand");
 	group.setGoalTolerance(.05);
 	group.setPoseTarget(target_pose1);
-	// Now, we call the planner to compute the plan
-	// and visualize it.
-	// Note that we are just planning, not asking move_group
-	// to actually move the robot.
-	moveit::planning_interface::MoveGroup::Plan my_plan;
-	bool success = group.plan(my_plan);
-	ROS_INFO("Visualizing plan 1 (pose goal) %s",success?"":"FAILED");
-	/* Sleep to give Rviz time to visualize the plan. */
-	sleep(5.0);
-	// Visualizing plans
-	// ^^^^^^^^^^^^^^^^^
+	*/
+	
+
+	//object placement
+	moveit_msgs::CollisionObject collision_object;
+	//collision_object.header.frame_id = group.getPlanningFrame();
+	collision_object.header.frame_id = "mico_link_base";	
+	collision_object.id = "box1";
+
+	/* Define a box to add to the world. */
+	shape_msgs::SolidPrimitive primitive;
+	primitive.type = primitive.BOX;
+	primitive.dimensions.resize(3);
+	primitive.dimensions[0] = 0.1;
+	primitive.dimensions[1] = 0.4;
+	primitive.dimensions[2] = 1.0;
+
+	/* A pose for the box (specified relative to frame_id) */
+	geometry_msgs::Pose box_pose;
+	box_pose.orientation.w = 1.0;
+	box_pose.position.x =  0.0;
+	box_pose.position.y =  0.4;
+	box_pose.position.z =  0.0;
+
+	collision_object.primitives.push_back(primitive);
+	collision_object.primitive_poses.push_back(box_pose);
+	collision_object.operation = collision_object.ADD;
+
+	std::vector<moveit_msgs::CollisionObject> collision_objects;
+	collision_objects.push_back(collision_object);
+	
+	//finally, add the collision objects to planning environment
+
+	sleep(1.0);
+	char in;
+	while(in != 'q'){
+		planning_scene_interface.addCollisionObjects(collision_objects);
+		group.setPlanningTime(10.0); //10 second maximum for collision computation
+		std::vector<double> q_vals;
+		std::cout << "Please specify 6 joint angles for a custom plan. Alternatively, enter the number of a preset." << std::endl;
+		for(int i = 1; i < 7; i++){
+			double in_q;
+			std::cout << std::endl << "Angle " << i << ": ";
+			std::cin >> in_q;
+			if(!std::cin.good()){
+				std::cout << "Invalid input, please try again." << std::endl;
+				i -= 1;
+				//std::cin.clear();
+			}
+			else
+				q_vals.push_back(in_q);
+			std::cin.clear();
+		}
+		//std::cout << "[1] - Home" << std::endl;
+		//std::cout << "[2] - Ready" << std::endl;
+		
+		//joint angles
+		//q_vals += 20.0, 20.0, 20.0, 20.0, 20.0, 20.0;
+		group.setStartState(*group.getCurrentState());
+		group.setJointValueTarget(q_vals);
+		//bool success = group.plan(my_plan);
+		// Now, we call the planner to compute the plan
+		// and visualize it.
+		// Note that we are just planning, not asking move_group
+		// to actually move the robot.
+		moveit::planning_interface::MoveGroup::Plan my_plan;
+		bool success = group.plan(my_plan);
+		ROS_INFO("Visualizing plan 1 (pose goal) %s",success?"":"FAILED");
+		std::cout << "Enter 'q' to quit, or 'a' to do another: ";
+		std::cin >> in;
+		std::cout<< std::endl;
+	}
+		// Visualizing plans
+		// ^^^^^^^^^^^^^^^^^
 	// Now that we have a plan we can visualize it in Rviz. This is not
 	// necessary because the group.plan() call we made above did this
 	// automatically. But explicitly publishing plans is useful in cases that we
 	// want to visualize a previously created plan.
-	if (1)
-	{
-		ROS_INFO("Visualizing plan 1 (again)");
-		display_trajectory.trajectory_start = my_plan.start_state_;
-		display_trajectory.trajectory.push_back(my_plan.trajectory_);
-		display_publisher.publish(display_trajectory);
+	//if (1)
+	//{
+	//	ROS_INFO("Visualizing plan 1 (again)");
+	//	display_trajectory.trajectory_start = my_plan.start_state_;
+	//	display_trajectory.trajectory.push_back(my_plan.trajectory_);
+	//	display_publisher.publish(display_trajectory);
 		/* Sleep to give Rviz time to visualize the plan. */
-		sleep(5.0);
-	}
+	//	sleep(5.0);
+	//}
 
 
 	/*
