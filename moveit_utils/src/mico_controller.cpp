49ba8e9fd48898647e8eb9cb1d9af89c146e5306
maxsvetlik <maxsvetlik@gmail.com>
2015-04-23 18:04:16 -0500

    added additional debug output

--- moveit_utils/src/mico_controller.cpp
+++ moveit_utils/src/mico_controller.cpp
@@ -16,7 +16,7 @@
 
 using namespace std;
 static std::vector<vector<float> > trajectory;
-
+sensor_msgs::JointState current_jpos;
 //true if Ctrl-C is pressed
 bool g_caught_sigint=false;
 bool recording = false;
@@ -29,21 +29,30 @@ void sig_handler(int sig)
 	ros::shutdown();
 	exit(1);
 };
+//Joint state cb
+void joint_state_cb (const sensor_msgs::JointStateConstPtr& input)
+{
+	current_jpos = *input;
+};
 bool cb(moveit_utils::MicoController::Request &req, moveit_utils::MicoController::Response &res){
 	
 	actionlib::SimpleActionClient<jaco_msgs::ArmJointAnglesAction> ac("/mico_arm_driver/joint_angles/arm_joint_angles", true);
 	trajectory_msgs::JointTrajectory trajectory = req.trajectory.joint_trajectory;
 	double q1,q2,q3,q4,q5,q6;
 	jaco_msgs::ArmJointAnglesGoal goal;
+	char dumb;
 	for(int i = 1; i < trajectory.points.size(); i++){ //skip first message because it always rotates j6 2pi
 		//set this goal's qvals
+		ros::spinOnce();
 		q1 = trajectory.points.at(i).positions.at(0);
 		q2 = trajectory.points.at(i).positions.at(1);
 		q3 = trajectory.points.at(i).positions.at(2);
 		q4 = trajectory.points.at(i).positions.at(3);
 		q5 = trajectory.points.at(i).positions.at(4);
 		q6 = trajectory.points.at(i).positions.at(5);
+		ROS_INFO("Current position: %f, %f, %f, %f, %f, %f", current_jpos.position[0], current_jpos.position[1], current_jpos.position[2], current_jpos.position[3], current_jpos.position[4], current_jpos.position[5]); 
 		ROS_INFO("Target position: %f, %f, %f, %f, %f, %f",q1,q2,q3,q4,q5,q6);
+		cin >> dumb;
 		goal.angles.joint1 = q1;
 		goal.angles.joint2 = q2;
 		goal.angles.joint3 = q3;
@@ -57,6 +66,7 @@ bool cb(moveit_utils::MicoController::Request &req, moveit_utils::MicoController
 		std::cout << ac.getState().toString() << std::endl;
 	}
 	ROS_INFO("Done with playback.");
+	ROS_INFO("Waiting...");
 	res.done = true;
 	return true;
 }
@@ -69,6 +79,9 @@ int main(int argc, char** argv)
 	bool done = false;
 	char input;
 	ros::NodeHandle n;
+	//listen to joint position
+	ros::Subscriber sub_angles = n.subscribe ("/joint_states", 1, joint_state_cb);
+
 	
 	ros::ServiceServer srv = n.advertiseService("mico_controller", cb);
 	ROS_INFO("Service loaded");
