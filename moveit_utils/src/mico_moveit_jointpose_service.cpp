11ceec2c75dceb36d083739e862af798f29a8b9d
maxsvetlik <maxsvetlik@gmail.com>
2015-05-01 00:33:52 -0500

    rough draft of service in place

--- moveit_utils/src/mico_moveit_jointpose_service.cpp
+++ moveit_utils/src/mico_moveit_jointpose_service.cpp
@@ -1,4 +1,4 @@
-nclude <signal.h>
+#include <signal.h>
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <moveit/move_group_interface/move_group.h>
@@ -13,14 +13,15 @@ nclude <signal.h>
 #include "moveit_utils/MicoController.h"
 #include "ros/ros.h"
 #include "geometry_msgs/Quaternion.h"
+#include "moveit_utils/MicoMoveitJointPose.h"
 using namespace boost::assign;
     
 #define NUM_JOINTS 6
 
-
 bool g_caught_sigint = false;
 std::vector<double> q_vals;
 
+ros::ServiceClient controller_client;
 
 void sig_handler(int sig){
     g_caught_sigint = true;
@@ -28,10 +29,14 @@ void sig_handler(int sig){
     ros::shutdown();
     exit(1);
 };  
-void toolpos_cb(const geometry_msgs::PoseStamped &msg){
-    q = msg.pose.orientation;
-}
-void service_cb(moveit_utils::MicoMoveitJointPose::Request &req, moveit_utils::MicoMoveitJointPose::Response &res){
+bool service_cb(moveit_utils::MicoMoveitJointPose::Request &req, moveit_utils::MicoMoveitJointPose::Response &res){
+    moveit_utils::MicoController srv_controller;
+    moveit::planning_interface::MoveGroup group("arm");
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;   
+    group.setPlanningTime(5.0); //10 second maximum for collision computation
+
+
+    q_vals.clear();
     for(int i = 0; i < NUM_JOINTS; i++){
         switch(i) {
             case 1  :    q_vals.push_back(req.target.joint1); break;
@@ -44,44 +49,35 @@ void service_cb(moveit_utils::MicoMoveitJointPose::Request &req, moveit_utils::M
     }
     group.setJointValueTarget(q_vals);
     group.setStartState(*group.getCurrentState());
-    //publish pose
-    geometry_msgs::PoseStamped stampOut;
     moveit::planning_interface::MoveGroup::Plan my_plan;
     bool success = group.plan(my_plan);
     //call service
     moveit_utils::MicoController srv;
-    srv.request.trajectory = my_plan.trajectory_;
-    if(client.call(srv)){
+    srv_controller.request.trajectory = my_plan.trajectory_;
+    if(controller_client.call(srv_controller)){
        ROS_INFO("Service call sent. Prepare for movement.");
-       res.completed = req.done;
+       res.completed = srv_controller.response.done;
     }
     else {
        ROS_INFO("Service call failed. Is the service running?");
        res.completed = false;
     }
+    return true;
 }   
 int main(int argc, char **argv)
 {   
     ros::init(argc, argv, "move_group_interface_tutorial");
-    ros::NodeHandle node_handle;
+    ros::NodeHandle nh;
     ros::AsyncSpinner spinner(1);
     spinner.start();
-    
-    //button position publisher
-    ros::Publisher pose_pub = node_handle.advertise<geometry_msgs::PoseStamped>("target_trajectory/pose", 10);
     //make controller service
     //TODO: as a rosparam, option to use different controllers
-    ros::ServiceClient client = node_handle.serviceClient<moveit_utils::MicoVelocityController>("mico_velocity_controller");
-    //subscribers
-    ros::Subscriber sub_tool = node_handle.subscribe("/mico_arm_driver/out/tool_position", 1, toolpos_cb);
-    moveit::planning_interface::MoveGroup group("arm"); //this is the specific group name you'd like to move
-    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-    // Create a publisher for visualizing plans in Rviz.
-    ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/       display_planned_path", 1, true);
-    //TODO: as a rosparam, option for planning time
-    group.setPlanningTime(5.0); //10 second maximum for collision computation
+    //ros::ServiceClient client = nh.serviceClient<moveit_utils::MicoMoveitJointPose>("mico_moveit_joint_pose");
+    controller_client = nh.serviceClient<moveit_utils::MicoController>("mico_velocity_controller");
+    ros::ServiceServer srv = nh.advertiseService("mico_jointpose_service", service_cb);
 
-    ros::shutdown();
+    //TODO: as a rosparam, option for planning time
+    ros::spin();
     return 0;
 }
  
