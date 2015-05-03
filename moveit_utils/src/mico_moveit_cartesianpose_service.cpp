889e910e903ba5823bee42709b2635cbbb253d34
maxsvetlik <maxsvetlik@gmail.com>
2015-05-03 18:33:46 -0500

    added service for cartesian joint space goals to interface with moveit

---
+++ moveit_utils/src/mico_moveit_cartesianpose_service.cpp
@@ -0,0 +1,68 @@
+#include <signal.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <moveit/move_group_interface/move_group.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+//services
+#include "moveit_utils/MicoController.h"
+#include "ros/ros.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "moveit_utils/MicoMoveitCartesianPose.h"
+
+bool g_caught_sigint = false;
+std::vector<double> q_vals;
+
+ros::ServiceClient controller_client;
+
+void sig_handler(int sig){
+    g_caught_sigint = true;
+    ROS_INFO("caugt sigint, init shutdown seq...");
+    ros::shutdown();
+    exit(1);
+};
+bool service_cb(moveit_utils::MicoMoveitCartesianPose::Request &req, moveit_utils::MicoMoveitCartesianPose::Response &res){
+    moveit_utils::MicoController srv_controller;
+    moveit::planning_interface::MoveGroup group("arm");
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+    group.setPlanningTime(5.0); //5 second maximum for collision computation
+    group.setPoseTarget(req.target);
+    group.setStartState(*group.getCurrentState());
+    moveit::planning_interface::MoveGroup::Plan my_plan;
+    bool success = group.plan(my_plan);
+    ROS_INFO("planning success: %c", success);
+    //call service
+    moveit_utils::MicoController srv;
+    srv_controller.request.trajectory = my_plan.trajectory_;
+    if(controller_client.call(srv_controller)){
+       ROS_INFO("Service call sent. Prepare for movement.");
+       res.completed = srv_controller.response.done;
+    }
+    else {
+       ROS_INFO("Service call failed. Is the service running?");
+       res.completed = false;
+    }
+    ros::spinOnce();
+    return true;
+}
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "mico_moveit_cartesianpose_service");
+    ros::NodeHandle nh;
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+    //make controller service
+    //TODO: as a rosparam, option to use different controllers
+    //ros::ServiceClient client = nh.serviceClient<moveit_utils::MicoMoveitJointPose>("mico_moveit_joint_pose");
+    controller_client = nh.serviceClient<moveit_utils::MicoController>("mico_controller");
+    ros::ServiceServer srv = nh.advertiseService("mico_cartesiantpose_service", service_cb);
+
+    //TODO: as a rosparam, option for planning time
+    ros::spin();
+    return 0;
+}
+
+
