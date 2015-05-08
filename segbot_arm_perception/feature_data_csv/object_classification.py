53695f81b6683795f84ab8107e2dca4bafdf9696
William Xie <wxie@cs.utexas.edu>
2015-05-07 23:53:13 -0500

    feature to classifier label working

--- segbot_arm_perception/feature_data_csv/object_classification.py
+++ segbot_arm_perception/feature_data_csv/object_classification.py
@@ -8,33 +8,53 @@ from sklearn import neighbors, datasets
 import csv
 import numpy
 
+
+def get_classifer_vector(classifier_name, object_label_vector):
+    classifier_object_vector = []
+    classifier_vector = []
+    # Retrieve classifiers for each object
+    with open("object_classifier_table.csv", "r") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            classifier_object_vector.append(row[classifier_name])
+
+    # Map each classifier value to each feature
+    for object_label in object_label_vector:
+        classifier_vector.append(classifier_object_vector[object_label - 1])
+    return classifier_vector
+
 if __name__ == '__main__':
+    # Input parameters
+    classifier_name = 'red'
     n_neighbors = 3
 
     # Read feature vectors
     reader = csv.reader(open("train.csv", "r"), delimiter=',')
-    X = np.array(list(reader))
-    X = X.astype(np.float64)
+    feature_vector_list = np.array(list(reader))
+    feature_vector_list = feature_vector_list.astype(np.float64)
     # Read object labels
     reader = csv.reader(open("train_object_index.csv", "r"))
-    y = np.array(list(reader))
-    # y = y.astype(np.int64)
-    y = np.squeeze(y)
-    print(X.shape)
-    print(y.shape)
+    object_label_vector = np.array(list(reader))
+    object_label_vector = np.squeeze(object_label_vector)
+    object_label_vector = object_label_vector.astype(np.int32)
     # Read classifier list
-    # with open("object_classifier_table.csv", "r") as csvfile:
-    #     reader = csv
-    reader = csv.reader(open("object_classifiers.csv", "r"), delimiter=',')
-    classifier_table = np.array(list(reader))
+    classifier_vector = np.array(get_classifer_vector(classifier_name, object_label_vector))
+    print(feature_vector_list.shape)
+    print(object_label_vector.shape)
+    print(classifier_vector.shape)
+
+    for i in range(len(feature_vector_list)):
+        print("{0}\t{1}".format(object_label_vector[i], classifier_vector[i]))
+
+
 
     # Map feature object pairs
 
     # Create model
     clf = neighbors.KNeighborsClassifier(n_neighbors, weights = 'uniform')
-    clf.fit(X[:][:-11], y[:-11])
-    prediction = clf.predict(X[:][-11:])
+    clf.fit(feature_vector_list[:][:-11], classifier_vector[:-11])
+    prediction = clf.predict(feature_vector_list[:][-11:])
     print("predictions:")
     print(prediction)
     print("expected:")
-    print(y[-11:])
+    print(classifier_vector[-11:])
