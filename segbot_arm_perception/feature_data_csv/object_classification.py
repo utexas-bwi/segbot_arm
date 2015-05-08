d4944b0566c4b7bbe6565027a46a7c8d19b54a7a
William Xie <wxie@cs.utexas.edu>
2015-05-08 00:30:59 -0500

    subset-ing working

--- segbot_arm_perception/feature_data_csv/object_classification.py
+++ segbot_arm_perception/feature_data_csv/object_classification.py
@@ -23,6 +23,28 @@ def get_classifer_vector(classifier_name, object_label_vector):
         classifier_vector.append(classifier_object_vector[object_label - 1])
     return classifier_vector
 
+# Subset object labels are the object number for
+def subset_feature_vectors_by_object_label(feature_vector_list, object_label_vector,
+                                           classifier_vector, subset_object_labels):
+    feature_vector_subset_index_list = []
+    feature_vector_sublist = []
+    object_label_sublist = []
+    classifier_sublist = []
+
+    # Get find all the feature indices for desired objects
+    for i in range(len(object_label_vector)):
+        if object_label_vector[i] in subset_object_labels:
+            feature_vector_subset_index_list.append(i)
+
+    # Construct new sublists
+    for i in feature_vector_subset_index_list:
+        feature_vector_sublist.append(feature_vector_list[i])
+        object_label_sublist.append(object_label_vector[i])
+        classifier_sublist.append(classifier_vector[i])
+
+    return (feature_vector_sublist, object_label_sublist, classifier_sublist)
+
+
 if __name__ == '__main__':
     # Input parameters
     classifier_name = 'red'
@@ -32,23 +54,26 @@ if __name__ == '__main__':
     reader = csv.reader(open("train.csv", "r"), delimiter=',')
     feature_vector_list = np.array(list(reader))
     feature_vector_list = feature_vector_list.astype(np.float64)
-    # Read object labels
+    # Read object labels (object index starts from 1)
     reader = csv.reader(open("train_object_index.csv", "r"))
     object_label_vector = np.array(list(reader))
     object_label_vector = np.squeeze(object_label_vector)
     object_label_vector = object_label_vector.astype(np.int32)
     # Read classifier list
     classifier_vector = np.array(get_classifer_vector(classifier_name, object_label_vector))
+    classifier_vector = classifier_vector.astype(np.int32)
     print(feature_vector_list.shape)
     print(object_label_vector.shape)
     print(classifier_vector.shape)
 
-    for i in range(len(feature_vector_list)):
-        print("{0}\t{1}".format(object_label_vector[i], classifier_vector[i]))
+    # Get a subset of the features based on object
+    feature_vector_sublist, object_label_sublist, classifier_sublist = subset_feature_vectors_by_object_label(feature_vector_list, object_label_vector, classifier_vector, [46, 47])
 
+    print(object_label_sublist)
+    print(classifier_sublist)
 
-
-    # Map feature object pairs
+    # for i in range(len(feature_vector_list)):
+    #     print("{0}\t{1}".format(object_label_vector[i], classifier_vector[i]))
 
     # Create model
     clf = neighbors.KNeighborsClassifier(n_neighbors, weights = 'uniform')
