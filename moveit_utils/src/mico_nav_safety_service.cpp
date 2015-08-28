e974e3e6eba62d5711cd3cf881bd6e3475f79f1e
maxsvetlik <maxsvetlik@gmail.com>
2015-08-28 13:04:37 -0500

    fixed issue with transform listener declaration and timing

--- moveit_utils/src/mico_nav_safety_service.cpp
+++ moveit_utils/src/mico_nav_safety_service.cpp
@@ -44,8 +44,7 @@ position: [-2.1993071377437037, -1.3080344725778785, 0.9615352436795538, -2.4799
 bool g_caught_sigint = false;
 std::vector<double> q_safe;
 ros::ServiceClient movement_client;
-ros::Publisher pub;
-tf::TransformListener listener;
+ros::Publisher pub_;
 double inflationRad;
 bool safe = true;
 std_msgs::Bool pub_data; 
@@ -58,10 +57,13 @@ void sig_handler(int sig){
 };
 
 void joint_state_cb(const sensor_msgs::JointStateConstPtr& js){
+	tf::TransformListener listener;
     if (js->position.size() > 4){ //Message from the base or the arm
         tf::StampedTransform transform;
         try{
-          listener.lookupTransform("/mico_base", "/base_link",  
+          listener.waitForTransform("/mico_link_base", "/base_link",  
+                                   ros::Time(0), ros::Duration(1.0));
+          listener.lookupTransform("/mico_link_base", "/base_link",  
                                    ros::Time(0), transform);
         }
         catch (tf::TransformException ex){
@@ -73,11 +75,14 @@ void joint_state_cb(const sensor_msgs::JointStateConstPtr& js){
         listener.transformPose("mico_api_origin", stampedPose, stampOut);
 */
         for(int i = 0; i < 6; i++){ //check if all joints are within robot radius
+			ROS_INFO("Transform x: %f", js->position.at(i)*transform.getOrigin().x());
+			ROS_INFO("Transform y: %f", js->position.at(i)*transform.getOrigin().y());
+
             safe &= abs(js->position.at(i)*transform.getOrigin().x()) < inflationRad && 
                     abs(js->position.at(i)*transform.getOrigin().y()) < inflationRad;
         }
         pub_data.data = safe;
-        pub.publish(pub_data);
+        pub_.publish(pub_data);
     }
 };
 
@@ -108,14 +113,14 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
     movement_client = nh.serviceClient<moveit_utils::MicoMoveitJointPose>("mico_jointpose_service");
     ros::Subscriber sub_angles = nh.subscribe ("/joint_states", 1, joint_state_cb);
-    pub = nh.advertise<std_msgs::Bool>("mico_nav_safe", 10);
+    pub_ = nh.advertise<std_msgs::Bool>("mico_nav_safe", 10);
     ros::ServiceServer srv = nh.advertiseService("mico_nav_safety", service_cb);
 
 
 
     q_safe += 2.86,-1.88,-0.198,-1.634,-0.1,2.49; //defines the 'go-to' safe position
 
-    nh.param("inflation_radius", inflationRad, .5);
+    nh.param("inflation_radius", inflationRad, .1);
 
     ros::spin();
     return 0;
