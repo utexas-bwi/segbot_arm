1f50cbf0d9fc87b5aa6ade7553b832d6da1213e4
Benjamin Singer <benjamin.z.singer@gmail.com>
2016-11-29 12:56:27 -0600

    Draft of bash file

--- bagfiles/joy/capture_csv.sh
+++ bagfiles/joy/capture_csv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo Recording joy
+echo Recording joy. Press Ctrl-C when recording is completed
 rosbag record joy
-read -rsp $'Press any key when data recording is completed...\n' -n1 key
-rostopic echo -b *.bag -p /joy
+read -rsp $'Press any key to continue...\n' -n1 key
+for i in $(ls *.bag); do rostopic echo -b $i -p joy > $i.csv; done
