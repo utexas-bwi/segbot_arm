4d1c656dc9bf6665ebabca5f1e5eb73124552198
Benjamin Singer <benjamin.z.singer@gmail.com>
2016-11-29 14:09:47 -0600

    Added all files

--- bagfiles/capture_csv_all.sh
+++ bagfiles/capture_csv_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-x-terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joint_states/joint_states_capture_csv.sh &
+-terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joint_states/joint_states_capture_csv.sh &
 x-terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joint_states/tool_position_capture_csv.sh &
 x-terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joint_states/joint_efforts_capture_csv.sh &
 x-terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joint_states/finger_position_capture_csv.sh
