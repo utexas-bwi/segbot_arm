3b6980d4beec649854a2699d5558ad985b8f51af
Benjamin Singer <benjamin.z.singer@gmail.com>
2016-11-29 15:41:19 -0600

    Added PCD Recording

--- bagfiles/capture_csv_all.sh
+++ bagfiles/capture_csv_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
--terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joint_states/joint_states_capture_csv.sh &
-x-terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joint_states/tool_position_capture_csv.sh &
-x-terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joint_states/joint_efforts_capture_csv.sh &
-x-terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joint_states/finger_position_capture_csv.sh
+x-terminal-emulator -e /home/users/fri/lafd_ws/src/joystick_bwi/bagfiles/joint_states/joint_states_capture_csv.sh &
+x-terminal-emulator -e /home/users/fri/lafd_ws/src/joystick_bwi/bagfiles/tool_position/tool_position_capture_csv.sh &
+x-terminal-emulator -e /home/users/fri/lafd_ws/src/joystick_bwi/bagfiles/joint_efforts/joint_efforts_capture_csv.sh &
+x-terminal-emulator -e /home/users/fri/lafd_ws/src/joystick_bwi/bagfiles/finger_position/finger_position_capture_csv.sh &
+x-terminal-emulator -e /home/users/fri/lafd_ws/src/joystick_bwi/bagfiles/point_cloud/point_cloud_capture_pcd.sh 
