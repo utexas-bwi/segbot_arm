dbc2ccbcea01f0e77c83728c87f72c381ad48ff2
Benjamin Singer <benjamin.z.singer@gmail.com>
2016-11-29 14:08:58 -0600

    Made small changes to bash scripts

--- bagfiles/joint_states/joint_states_capture_csv.sh
+++ bagfiles/joint_states/joint_states_capture_csv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo Recording joint_states. Press Ctrl-C when recording is completed
+cd joint_states
 rosbag record joint_states
 read -rsp $'Press any key to continue...\n' -n1 key
 for i in $(ls *.bag); do rostopic echo -b $i -p joint_states > $i.csv; done
