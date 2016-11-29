6b36813bacd5345f2c12741c4a34e0a81af0a5b0
Benjamin Singer <benjamin.z.singer@gmail.com>
2016-11-29 13:54:26 -0600

    Added bash scripts to collect all arm info

--- bagfiles/capture_csv_all.sh
+++ bagfiles/capture_csv_all.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-x-terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joy/joy_capture_csv.sh
-x-terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joint_states/joint_states_capture_csv.sh
+x-terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joint_states/joint_states_capture_csv.sh &
+x-terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joint_states/tool_position_capture_csv.sh &
+x-terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joint_states/joint_efforts_capture_csv.sh &
+x-terminal-emulator -e /home/bzsinger/catkin_ws/src/joystick_bwi/bagfiles/joint_states/finger_position_capture_csv.sh
