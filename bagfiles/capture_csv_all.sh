6f9baf7d93d381be45e799b39bcba10f781b50e9
Benjamin Singer <benjamin.z.singer@gmail.com>
2016-12-01 12:49:12 -0600

    Added scripts to collect images

--- bagfiles/capture_csv_all.sh
+++ bagfiles/capture_csv_all.sh
@@ -3,4 +3,5 @@ x-terminal-emulator -e /home/users/fri/lafd_ws/src/joystick_bwi/bagfiles/joint_s
 x-terminal-emulator -e /home/users/fri/lafd_ws/src/joystick_bwi/bagfiles/tool_position/tool_position_capture_csv.sh &
 x-terminal-emulator -e /home/users/fri/lafd_ws/src/joystick_bwi/bagfiles/joint_efforts/joint_efforts_capture_csv.sh &
 x-terminal-emulator -e /home/users/fri/lafd_ws/src/joystick_bwi/bagfiles/finger_position/finger_position_capture_csv.sh &
-x-terminal-emulator -e /home/users/fri/lafd_ws/src/joystick_bwi/bagfiles/point_cloud/point_cloud_capture_pcd.sh 
+x-terminal-emulator -e /home/users/fri/lafd_ws/src/joystick_bwi/bagfiles/point_cloud/point_cloud_capture_pcd.sh &
+x-terminal-emulator -e /home/users/fri/lafd_ws/src/joystick_bwi/bagfiles/xtion_camera/xtion_camera_capture_image.sh 
