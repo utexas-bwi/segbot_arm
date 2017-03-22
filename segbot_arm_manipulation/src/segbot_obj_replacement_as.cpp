8569b9202d79f9cb3bde5aa89e4aa8eb10a921a4
Kathryn Baldauf <kathrynbaldauf@utexas.edu>
2017-03-22 15:43:23 -0500

    basic steps completed

--- segbot_arm_manipulation/src/segbot_obj_replacement_as.cpp
+++ segbot_arm_manipulation/src/segbot_obj_replacement_as.cpp
@@ -99,7 +99,7 @@ public:
 		sub_finger = nh_.subscribe("/mico_arm_driver/out/finger_position", 1, &ObjReplacementActionServer::fingers_cb, this);
 	  
 		//subscriber for grasps
-		sub_grasps = nh_.subscribe("/find_grasps/grasps_handles",1, &ObjReplacementActionServer::grasps_cb,this);  
+		//sub_grasps = nh_.subscribe("/find_grasps/grasps_handles",1, &ObjReplacementActionServer::grasps_cb,this);  
 
     	ROS_INFO("Starting replacement grasp action server..."); 
     	as_.start(); 
@@ -168,10 +168,11 @@ public:
 	}
 	
 	/*Assumptions: the robot has already approached the table, 
-	 * an object is in hand, the arm is currently still in safety mode*/
+	 * an object is in hand, the arm is currently still in safety mode,
+	 * cloud is organized and dense*/
 	void executeCB(const segbot_arm_manipulation::ObjReplacementGoalConstPtr &goal){
 		//step1: get the table scene, check validity
-		segbot_arm_perception::TabletopPerception::Response table_scene = segbot_arm_manipulation::getTabletopScene(n);
+		segbot_arm_perception::TabletopPerception::Response table_scene = segbot_arm_manipulation::getTabletopScene(nh_);
 		
 		if ((int)table_scene.cloud_clusters.size() == 0){
 			ROS_ERROR("[segbot_arm_replacement_as] a table must be present");
@@ -180,17 +181,28 @@ public:
 			as_.setAborted(result_);
 			return;
 		}
+		sensor_msgs::PointCloud2 plane = table_scene.cloud_plane;
+		
 		
 		//(later) step2: get information about the object
 		
 		//step3: cropbox filter to reduce size of points to check
 		
 		
-		//step4: voxel grid filter with a distance size of 5cm
-		sensor_msgs::PointCloud2 scene_plane;
-		PointCloudT::Ptr pcl_scene_plane (new PointCloudT);
-		pcl::fromROSMsg(scene_plane, pcl_scene_plane);
+		//transform the table scene point cloud 
+		sensor_msgs::PointCloud scene_plane;
+		listener.waitForTransform(plane.header.frame_id, "mico_link_base", ros::Time(0), ros::Duration(3.0));
+		sensor_msgs::convertPointCloud2ToPointCloud(plane, scene_plane);
+		listener.transformPointCloud("mico_link_base", scene_plane ,scene_plane); 
 		
+		//update the plane cloud to the transformed cloud
+		sensor_msgs::convertPointCloudToPointCloud2(scene_plane, plane);
+		
+		//get a pcl point cloud of the given plane
+		PointCloudT::Ptr pcl_scene_plane (new PointCloudT);
+		pcl::fromROSMsg(plane, *pcl_scene_plane);
+		
+		//step4: voxel grid filter with a distance size of 5cm
 		PointCloudT::Ptr plane_down_sam (new PointCloudT);
 		downsample_clouds(pcl_scene_plane, plane_down_sam, 0.05f);
 		
@@ -198,9 +210,45 @@ public:
 			//get a z value some height above the plane
 			//add some value to this to create a goal point 
 			//check the IK, if possible go to location
+		
+		int num_points = (int) plane_down_sam->points.size();
+		if ((int)num_points == 0){
+			ROS_ERROR("[segbot_arm_replacement_as] the table contains no points");
+			result_.success = false;
+			result_.error_msg = "down sampled cloud must still have points";
+			as_.setAborted(result_);
+			return;
+		}
+		
+		
+		//for now go through points like normal
+		for(int ind = 0; ind < num_points; ind++){
+			//create the current goal for the arm to move to when resetting
+			geometry_msgs::PoseStamped current_goal;
+			current_goal.header.frame_id = current_pose.header.frame_id;
+			current_goal.pose.position.x = plane_down_sam->points[ind].x;
+			current_goal.pose.position.y = plane_down_sam->points[ind].y;
+			current_goal.pose.position.z = plane_down_sam->points[ind].z + 0.1; //want it to be slightly above the table still
+			//for now use 0,0,0; determine a better quaternion 
+			current_goal.pose.orientation =  tf::createQuaternionMsgFromRollPitchYaw(0 , 0 , 0);
 			
-		//step6: ensure that the arm reached this location, release obj
+			moveit_msgs::GetPositionIK::Response ik_response_1 = segbot_arm_manipulation::computeIK(nh_,current_goal);
+			if (ik_response_1.error_code.val == 1){
+				//go to the position and for now, break
+				segbot_arm_manipulation::moveToPoseMoveIt(nh_, current_goal);
+				//to do: determine if the position was reached, if not retry?
+				break;
+			}
+
+		}
+		
+		//drop the object
+		segbot_arm_manipulation::openHand();
+		
 		//step7: home arm 
+		segbot_arm_manipulation::homeArm(nh_);
+		
+		//todo: set results based on if the location was reached and if the object is on the table
 		
 	}
 
