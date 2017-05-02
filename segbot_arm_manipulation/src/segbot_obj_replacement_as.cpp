01ba456f4cdd582a680841f6fbfbaa66c90e80dc
Kathryn Baldauf <kathrynbaldauf@utexas.edu>
2017-05-01 22:19:36 -0500

    clean up

--- segbot_arm_manipulation/src/segbot_obj_replacement_as.cpp
+++ segbot_arm_manipulation/src/segbot_obj_replacement_as.cpp
@@ -54,6 +54,7 @@ PointT sensor_origin;
 
 #define JOINT_RADIUS .195
 #define ABOVE_TABLE 0.1
+#define POSE_THRESHOLD 0.15
 
 
 
@@ -182,6 +183,7 @@ public:
 		grid.filter(*out_cloud);
 	}
 	
+	/*Function to find the distance between geometry_msgs points*/
 	float euclidean_distance(geometry_msgs::Point target , geometry_msgs::Point actual){
 		float x_diff = (float) target.x - actual.x;
 		float y_diff = (float) target.y - actual.y;
@@ -208,11 +210,12 @@ public:
 		return p1_diff < p2_diff;
 	}
 	
+	/*check if the position of the end effector is within some threshold of the target end effect position*/
 	bool check_if_reached(geometry_msgs::PoseStamped target_pose, geometry_msgs::PoseStamped actual_pose){
 		float distance =  euclidean_distance(target_pose.pose.position, actual_pose.pose.position);
 		
 		//TO DO: test this threshold
-		if(distance >= 0.25){
+		if(distance >= POSE_THRESHOLD){
 			//further than threshold centimeters away from goal location
 			return false;
 		}
@@ -237,16 +240,6 @@ public:
 		sensor_msgs::PointCloud2 plane = table_scene.cloud_plane;
 		
 		
-		//(later) step2: get information about the object
-		
-		//calculate z value
-		double z_value = 0; 
-        //move object into view
-
-
-		//step3: cropbox filter to reduce size of points to check
-		
-		
 		//transform the table scene point cloud 
 		sensor_msgs::PointCloud scene_plane;
 		listener.waitForTransform(plane.header.frame_id, "mico_link_base", ros::Time(0), ros::Duration(3.0));
@@ -260,11 +253,11 @@ public:
 		PointCloudT::Ptr pcl_scene_plane (new PointCloudT);
 		pcl::fromROSMsg(plane, *pcl_scene_plane);
 		
-		//step4: voxel grid filter with a distance size of 5cm
+		//step2: voxel grid filter with a distance size of 5cm
 		PointCloudT::Ptr plane_down_sam (new PointCloudT);
 		downsample_clouds(pcl_scene_plane, plane_down_sam, 0.05f);
 
-		//publish downsampled cloud
+		//publish downsampled cloud for debugging
 		sensor_msgs::PointCloud2 plane_down_ros;
 		pcl::toROSMsg(*plane_down_sam,plane_down_ros);
 		plane_down_ros.header.frame_id = plane_down_sam->header.frame_id;
@@ -280,36 +273,39 @@ public:
 			return;
 		}
 		
-		//TO DO: test this
+		//step3: sort the points in the point cloud by distance to camera
 		//set the pointT of the camera's sensor
-		Eigen::Vector4f origin = plane_down_sam->sensor_origin_; 
+		Eigen::Vector4f origin = plane_down_sam->sensor_origin_; //TO DO test this, make sure origin is set 
 		sensor_origin.x = origin(0);
 		sensor_origin.y = origin(1);
 		sensor_origin.z = origin(2);
 		
 		std::sort(plane_down_sam->points.begin(), plane_down_sam->points.end(), sort_hlp);
 		
-		//for now go through points iteratively
+		//step4: go through the points and find a place to set the object
 		for(int ind = 0; ind < num_points; ind++){
+			
 			//create the current goal for the arm to move to when resetting
 			geometry_msgs::PoseStamped current_goal;
 			current_goal.header.frame_id = current_pose.header.frame_id;
 			current_goal.pose.position.x = plane_down_sam->points[ind].x;
 			current_goal.pose.position.y = plane_down_sam->points[ind].y;
-			current_goal.pose.position.z = plane_down_sam->points[ind].z + ABOVE_TABLE; //want it to be slightly above the table still
+			
+			//set z value slightly above the table
+			current_goal.pose.position.z = plane_down_sam->points[ind].z + ABOVE_TABLE;
 
-			//TO DO: for now use the current quaternion
 			//current_goal.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(-3.14/2, 0, 0);
 			current_goal.pose.orientation =  current_pose.pose.orientation; //TO DO: test
 			
 			//check the inverse kinematics, if possible, move to the pose and drop object
 			moveit_msgs::GetPositionIK::Response ik_response_1 = segbot_arm_manipulation::computeIK(nh_,current_goal);
+			
 			if (ik_response_1.error_code.val == 1){
 				segbot_arm_manipulation::moveToPoseMoveIt(nh_, current_goal);
 				if(check_if_reached(current_goal, current_pose)){
 					//reached location, success
 					segbot_arm_manipulation::openHand();
-					result_.success = true; //TO DO: check if moving to location was successful
+					result_.success = true; 
 					break;
 				}
 				//did not reach location, continue trying 
@@ -317,10 +313,10 @@ public:
 			}
 		}
 		
-		//step7: home arm 
+		//step5: home arm 
 		segbot_arm_manipulation::homeArm(nh_);
 		
-		//todo: set results based on if the location was reached and if the object is on the table
+		//step6: set success of the action
 		as_.setSucceeded(result_);
 
 	}
